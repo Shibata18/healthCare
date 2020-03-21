70b7aecd3a52fe0b998dea4a79a38ec9e1ed2c09
Douglas <douglas.shibata11@gmail.com>
2020-03-21 18:48:14 +0000

    alterações

---
+++ frontend/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LandingPage(){
+    return (
+        <>
+            <div className='app'>
+                Página Inicial
+            </div>
+        </>
+    )
+}   
+
+export default LandingPage;

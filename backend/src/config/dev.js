bf869f128c479a382eda8adc760c5b8d65f84507
Shibata18 <douglas.shibata11@gmail.com>
2020-03-23 10:42:45 -0300

    reformulação

--- backend/src/config/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports={
-    mongoURI:'mongodb+srv://admin:admin@cluster0-tf64h.mongodb.net/test?retryWrites=true&w=majority',
-}
--- frontend/src/components/Config.js
+++
@@ -1,2 +0,0 @@
-export const USER_SERVER = '/api/users';
-export const CHAT_SERVER = '/api/chat';

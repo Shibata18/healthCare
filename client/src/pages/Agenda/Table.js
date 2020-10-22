1c1609f33803a2001b4bb79e5570a60f170728ae
Shibata18 <douglas.shibata11@gmail.com>
2020-10-22 09:32:09 -0300

    Retirando prontuário e organizando o código

--- client/src/pages/Agenda/Table.js
+++ client/src/pages/Agenda/Table.js
@@ -49,43 +49,17 @@ function DataTable(props) {
         <TableCell>{item.profissionalNome}</TableCell>
         <TableCell>{item.pacienteNome}</TableCell>
         <TableCell>{formattedDate}</TableCell>
-       {/* {ehMedico === 'true' ? <>
-        <TableCell>
-         <PDFDownloadLink document={
-                <Document>
-                  <Page size="A4">
-                    <Header />
-                    <View>
-                      <Text style={styles.data}>Dados do Paciente </Text>
-                      <Text style={styles.text}>Paciente : {item.paciente_cpf} </Text>
-                    </View>
-                    <View style={styles.body}>
-                      <Text style={styles.data}>Dados do Médico </Text>
-                      <Text style={styles.text}>Médico : {item.doctor_cpf} </Text>
-                      <Text style={styles.text}>Especialidade: </Text>
-                      <Text style={styles.text}></Text>
-                      <Text style={styles.text}>Registro: </Text>
-                      <Text style={styles.text}>Telefone: </Text>
-                      <Text style={styles.text}>Email: </Text>
-                      <Text style={styles.data}>Observações </Text>
-                      <Text style={styles.text}>{item.prontuario === null ? '' : item.prontuario.prontuario}</Text>
-                      <Text style={styles.data}>Data e Hora da Consulta</Text>
-                      <Text style={styles.text}>{formattedDate}</Text>
-                    </View>
-                    <Footer />
-                  </Page>
-            </Document>} fileName={`${date.toUTCString()}.pdf`}>
-            {item.prontuario === null ?
-              '' : ({ blob, url, loading, error }) => (loading ? 'Carregando os dados...' : 'Baixar Prontuário!')}
-          </PDFDownloadLink> 
-        </TableCell></>: <TableCell></TableCell>}*/}
-        <TableCell align='justify'>{horarioAgendado ? <Button href='/chat' color='primary'><DuoIcon /></Button> : <Button disabled color='inherit'><DuoIcon /></Button>}</TableCell>
+        <TableCell align='justify'>
+          {horarioAgendado ? <Button href='/chat' color='primary'><DuoIcon /></Button> :
+            <Button disabled color='inherit'><DuoIcon /></Button>}
+        </TableCell>
         {ehMedico === 'true' ?
           <TableCell>
             <div style={{ width: "10%" }}>
               <ModalForm buttonLabel="Editar" item={item} updateState={props.updateState} />
             </div>
-          </TableCell> : <TableCell></TableCell>}
+          </TableCell> :
+          <TableCell></TableCell>}
       </TableRow>
     )
   })
@@ -98,9 +72,7 @@ function DataTable(props) {
             <TableCell>Profissional</TableCell>
             <TableCell>Paciente</TableCell>
             <TableCell>Horario</TableCell>
-{/*             {ehMedico === 'true' ?
-              <TableCell>Prontuário</TableCell> : <TableCell></TableCell>}
- */}            <TableCell>Vídeo</TableCell>
+            <TableCell>Vídeo</TableCell>
             {ehMedico === 'true' ?
               <TableCell>Editar</TableCell> : <TableCell></TableCell>}
           </TableRow>
